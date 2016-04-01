ab8ca71b9c02086b8a28bb8ebfa80b9d196791ce
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-04-01 15:03:24 +0800

    Change shared directory into common

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'shared'],
+        modulesDirectories: ['node_modules', 'common'],
         extensions: ['', '.js', '.jsx']
     },
     module: {

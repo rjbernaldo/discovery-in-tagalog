763782cd2a89cd3c81643412049d98522d6bbe74
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-04-19 20:36:15 +0800

    Add initial dependencies.

---
+++ lib/api_contraints.rb
@@ -0,0 +1,10 @@
+class ApiContraints
+	def initialize(options)
+		@version = options[:version]
+		@default = options[:default]
+	end
+
+	def matches?(req)
+		@default || req.headers["Accept"].include?("application/vnd.marketplace.v#{@version}")
+	end
+end

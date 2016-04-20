550a0bafdd817abfec00446654e91c1498172899
Rj Bernaldo <rj@promisepay.com>
2016-04-20 14:20:53 +0800

    Add first user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  it 'should be valid' do
+    expect(@user).to respond_to(:email)
+    expect(@user).to respond_to(:password)
+    expect(@user).to respond_to(:password_confirmation)
+    expect(@user).to be_valid
+  end
 end

23441317e8451d95d5524c5330032414bd293f50
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-04-25 20:19:16 +0800

    Add product search functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ActiveRecord::Base
 
     scope :filter_by_title, -> (keyword) { where("lower(title) LIKE ?", "%#{keyword.downcase}%") }
     scope :recent, -> { order(:updated_at) }
+
+    def self.search(params = {})
+        products = params[:product_ids].present? ? Product.find(params[:product_ids]) : Product.all
+        products = products.filter_by_title(params[:keyword]) if params[:keyword]
+        products = products.recent(params[:recent]) if params[:recent].present?
+
+        products
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -49,4 +49,40 @@ RSpec.describe Product, type: :model do
             expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
         end
     end
+
+    describe ".search" do
+        before(:each) do
+            @product1 = FactoryGirl.create(:product, title: "AAA1", price: 4)
+            @product2 = FactoryGirl.create(:product, title: "AAA2", price: 3)
+            @product3 = FactoryGirl.create(:product, title: "BBB3", price: 2)
+            @product4 = FactoryGirl.create(:product, title: "BBB4", price: 1)
+        end
+
+        context "when title is 'CCC'" do
+            it "returns an empty array" do
+                search_hash = { keyword: 'CCC' }
+                expect(Product.search(search_hash).count).to eq(0)
+            end
+        end
+
+        context "when title 'BBB'" do
+            it "returns product4" do
+                search_hash = { keyword: 'BBB' }
+                expect(Product.search(search_hash)).to eq([@product3, @product4])
+            end
+        end
+
+        context "when an empty hash is sent" do
+            it "returns all products" do
+                expect(Product.search()).to eq([@product1, @product2, @product3, @product4])
+            end
+        end
+
+        context "when product_ids field is present" do
+            it "returns the relevant products" do
+                search_hash = { product_ids: [@product1.id, @product2.id]}
+                expect(Product.search(search_hash)).to eq([@product1, @product2])
+            end
+        end
+    end
 end

32090f5d1dd0eedc94a26c7f705cb7a105ecdc97
Rj Bernaldo <rj.bernaldo@gmail.com>
2016-04-25 19:35:26 +0800

    Add param for getting all products

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -22,20 +22,40 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     describe "GET index" do
         before(:each) do
             4.times { FactoryGirl.create(:product) }
-            get :index
         end
 
-        it "returns 4 records from the database" do
-            products_response = json_response[:products]
-            expect(products_response.count).to eq(4)
-            expect(response.response_code).to eq(200)
+        context "when product_ids parameter is not sent" do
+            before(:each) do
+                get :index
+            end
+
+            it "returns 4 records from the database" do
+                products_response = json_response[:products]
+                expect(products_response.count).to eq(4)
+                expect(response.response_code).to eq(200)
+            end
+
+            it "returns the user object into each product" do
+              products_response = json_response[:products]
+              products_response.each do |product_response|
+                expect(product_response[:user]).to be_truthy
+              end
+            end
         end
 
-        it "returns the user object into each product" do
-          products_response = json_response[:products]
-          products_response.each do |product_response|
-            expect(product_response[:user]).to be_truthy
-          end
+        context "when product_ids parameter is sent" do
+            before(:each) do
+                @user = FactoryGirl.create(:user)
+                3.times { FactoryGirl.create(:product, user: @user) }
+                get :index, product_ids: @user.product_ids
+            end
+
+            it "only returns the products that belong to the user" do
+                products_response = json_response[:products]
+                products_response.each do |product_response|
+                    expect(product_response[:user][:email]).to eq(@user.email)
+                end
+            end
         end
     end
 end
